2ad4d898d67823f86ab4593b93eb22c656ca463d
xiaoxion <esaurubio11@gmail.com>
2013-04-18 17:57:33 -0400

    CRD are done, working on U

--- Rubio_Esau_Project2/js/main.js
+++ Rubio_Esau_Project2/js/main.js
@@ -1,50 +1,134 @@
+// **Add Jobs page interactions**
 $(function(){
+
+    // Needed static variables
     var maintenanceTypes = [ "Cleaning" , "Painting", "Electric" , "Plumbing"],
         priority;
-    //errorMessage = ids('error');
 
-    //Add Category
-    var addCat = function(){
-        $("<select></select>")
-            .appendTo('#typeOfWork')
-            .attr("id","worktype")
-            .attr("name","worktype")
-            .attr("data-native-menu","false");
-        for(var i=0,n=maintenanceTypes.length; i<n ; i++){
-            $('<option></option>')
-                .appendTo('#worktype')
-                .attr("value", maintenanceTypes[i])
-                .text(maintenanceTypes[i])
-        }
-    };
-    addCat();
-});
+    // Add Select Option
+    $("<select></select>")
+        .appendTo('#typeOfWork')
+        .attr("id","worktype")
+        .attr("name","worktype")
+        .attr("data-native-menu","false");
+    for(var i=0,n=maintenanceTypes.length; i<n ; i++){
+        $('<option></option>')
+            .appendTo('#worktype')
+            .attr("value", maintenanceTypes[i])
+            .text(maintenanceTypes[i])
+    }
 
-
-$('#additem').on('pageinit', function(){
-    var aiform = $('#additemform');
-    aiform.validate({
+    // Validate the Data
+    $('#additemform').validate({
         invalidHandler: function(form, validator) {},
         submitHandler: function() {
-            var data = aiform.serializeArray();
-            storeData(data);
+            storeData($('#additemform').serializeArray());
         }
     });
+
+    //Store the Data
+    var storeData = function(data){
+        var d = new Date();
+        keyGen = d.getTime();
+        var userInput = {};
+        userInput.location = ["Location:" , data[0].value ];
+        userInput.worktype = ["Work Type:" , data[1].value ];
+        userInput.priority = ["Priority:" , data[2].value ];
+        userInput.people   = ["Workers Sent:" , data[3].value ];
+        userInput.finishby = ["Finish By:" , data[4].value ];
+        userInput.notes    = ["Notes:" , data[5].value ];
+        localStorage.setItem(keyGen , JSON.stringify(userInput));
+        alert("Job Saved!");
+        location.reload();
+    };
 });
 
- /*
-var storeData = function(data){
-    var d = new Date();
-    keyGen = d.getTime();
-    var userInput = {};
-    console.log(data);
-    userInput.location = ["Location:" , data[0].value ];
-    userInput.worktype = ["Work Type:" , data[1].value ];
-    userInput.priority = ["Priority:" , data[2].value ];
-    userInput.people   = ["Workers Sent:" , data[3].value ];
-    userInput.finishby = ["Finish By:" , data[4].value ];
-    userInput.notes    = ["Notes:" , data[5].value ];
-    localStorage.setItem(keyGen , JSON.stringify(userInput));
-    alert("Job Saved!");
-};
-*/
+// **Display Jobs Page Interactions**
+$(function(){
+
+    // Delete All Button
+    $('<button></button>')
+        .appendTo('#set')
+        .text("Delete All")
+        .on('click', function(){
+            if (localStorage.length===0) {
+                alert("No Jobs to clear!");
+            } else {
+                var areYouSure = confirm("Are you sure you want to clear all Jobs??");
+                if(areYouSure ==  true) {
+                    localStorage.clear();
+                    alert("Jobs Cleared!");
+                    location.reload();
+                }
+            }
+        });
+
+    // Generate Jobs from Local Storage
+    if(localStorage.length > 0) {
+        $('<ul></ul>')
+            .appendTo('#set')
+            .attr('data-role', 'listview')
+            .attr('data-inset', 'true')
+            .attr('id',"listofjobs");
+        for(var i=0, l=localStorage.length; i<l; i++){
+            var parsed = JSON.parse(localStorage.getItem(localStorage.key(i)));
+            $('<li></li>')
+                .appendTo("#listofjobs")
+                .attr("id","job"+i);
+            $('<h2></h2>')
+                .appendTo("#job"+i)
+                .text(parsed.location[1]);
+            for(var n in parsed){
+                $('<p></p>')
+                    .appendTo("#job"+i)
+                    .text(parsed[n][0] + " " + parsed[n][1]);
+            }
+
+            // Edit Link
+            $('<a></a>')
+                .appendTo('#job'+i)
+                .attr('href', '#additem')
+                .attr('data-role', 'button')
+                .attr('data-inline', 'true')
+                .text("Edit Job")
+                .on('click',function(i){
+                    var userInput = JSON.parse(localStorage.getItem(localStorage.key(i)));
+                    console.log(userInput);
+                    $('#location').value = userInput.location[1];
+                    console.log($('#location'));
+                    $('#worktype').value = userInput.worktype[1];
+                    $('#people').value = userInput.people[1];
+                    $('#finishby').value = userInput.finishby[1];
+                    $('#notes').value = userInput.notes[1];
+                    var rad = document.forms[0].priority;
+                    for (var i=0 , len=rad.length; i<len; i++) {
+                        if (rad[i].value == "High" && userInput.priority[1] == "High") {
+                            rad[i].setAttribute('checked' , 'checked')
+                        } else if (rad[i].value == "Medium" && userInput.priority[1] == "Medium") {
+                            rad[i].setAttribute('checked' , 'checked')
+                        } else if (rad[i].value == "Low" && userInput.priority[1] == "Low") {
+                            rad[i].setAttribute('checked' , 'checked')
+                        }
+                    }
+                });
+
+            // Delete Link
+            $('<a></a>')
+                .appendTo('#job'+i)
+                .attr('href', '#')
+                .attr('data-role', 'button')
+                .attr('data-inline', 'true')
+                .attr('data-icon', 'delete')
+                .on('click',function(i){
+                        ask = confirm('Are you Sure?');
+                    if (ask) {
+                        localStorage.removeItem(localStorage.key(i));
+                        location.reload();
+                        alert('Job Deleted!')
+                    }
+                });
+        }
+    } else {
+        $("#set").contents().text("No Jobs to Display!")
+    }
+});
